38b3731a23226436058002483a1e4d364e3649ed
Bruno Amorim <bruno.amorim@mblabs.com.br>
2023-12-28 19:45:52 -0300

    Initial Commit

---
+++ Maps/MapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsApp.swift
+//  Maps
+//
+//  Created by Bruno Amorim on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
