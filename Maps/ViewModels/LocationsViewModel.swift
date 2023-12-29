b76fbdbe7f1284cacf31cfb34c773b0d301d4b2e
amorimcode <bruno.amorim032@gmail.com>
2023-12-29 01:26:53 -0300

    new views

--- Maps/ViewModels/LocationsViewModel.swift
+++ Maps/ViewModels/LocationsViewModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 import MapKit
-
+import SwiftUI
 
 class LocationsViewModel: ObservableObject {
     
@@ -21,8 +21,12 @@ class LocationsViewModel: ObservableObject {
         }
     }
     
+    // current region on map
     @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
     let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
+    // show list of locations
+    @Published var showLocationsList: Bool = false
     
     init () {
         let locations  = LocationsDataService.locations
@@ -33,9 +37,24 @@ class LocationsViewModel: ObservableObject {
     
     
     private func updateMapRegion(location: Location) {
-        mapRegion = MKCoordinateRegion(
-            center: location.coordinates,
-            span: mapSpan
-        )
+        withAnimation(.easeInOut) {
+            mapRegion = MKCoordinateRegion(
+                center: location.coordinates,
+                span: mapSpan
+            )
+        }
+    }
+    
+    func toggleLocationsList() {
+        withAnimation(.easeInOut) {
+            showLocationsList.toggle()
+        }
+    }
+    
+    func showNextLocation(location: Location) {
+        withAnimation(.easeInOut) {
+            mapLocation = location
+            showLocationsList = false
+        }
     }
 }
