648a62bc78fceb5b0b91e3cbc3976862f933369d
amorimcode <bruno.amorim032@gmail.com>
2023-12-28 22:09:06 -0300

    Add Map to SwiftUI project with MapKit

--- Maps/ViewModels/LocationsViewModel.swift
+++ Maps/ViewModels/LocationsViewModel.swift
@@ -6,13 +6,36 @@
 //
 
 import Foundation
+import MapKit
 
 
 class LocationsViewModel: ObservableObject {
+    
+    // all loaded locations
     @Published var locations: [Location]
+    
+    // current location on the map
+    @Published var mapLocation: Location {
+        didSet {
+            
+        }
+    }
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init () {
         let locations  = LocationsDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first!
+        self.updateMapRegion(location: locations.first!)
+    }
+    
+    
+    private func updateMapRegion(location: Location) {
+        mapRegion = MKCoordinateRegion(
+            center: location.coordinates,
+            span: mapSpan
+        )
     }
 }
